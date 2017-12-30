62d53221db630c78c817a10ef072e2858fe2cc7d
RajuShetty <rajushetty.b@gmail.com>
2017-12-30 12:06:10 +0530

    admob added

--- js/admob.js
+++ js/admob.js
@@ -4,16 +4,16 @@ var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) {
   admobid = { // for Android
     banner: '',
-	interstitial: 'ca-app-pub-6251426836814427/3248594733',
-    rewardvideo: 'ca-app-pub-6251426836814427/3242168952',
+    interstitial: 'ca-app-pub-6251426836814427/8893790394',
+    rewardvideo: 'ca-app-pub-6251426836814427/2847256798',
   };
-}  /*else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
   admobid = { // for iOS
     banner: '',
-    interstitial: 'ca-app-pub-6251426836814427/7913997593',
-    rewardvideo: 'ca-app-pub-6251426836814427/9390730792',
+    interstitial: 'ca-app-pub-6251426836814427/4157585994',
+    rewardvideo: 'ca-app-pub-6251426836814427/5634319197',
   };
-}else {
+}/* else {
   admobid = { // for Windows Phone
     banner: 'ca-app-pub-6869992474017983/8878394753',
     interstitial: 'ca-app-pub-6869992474017983/1355127956',
@@ -37,13 +37,13 @@ function initApp() {
   // this will load a full screen ad on startup
   AdMob.prepareInterstitial({
     adId: admobid.interstitial,
-    //isTesting: true, // TODO: remove this line when release
+    
     autoShow: true
   });
   
   prepareRewardVideoAd({
-	  adId: admobid.rewardvideo, 
-	//isTesting: true, // TODO: remove this line when release
+	  adId: admobid.rewardvideo,
+  
     autoShow: true });
 }
 
